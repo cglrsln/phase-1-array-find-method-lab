3d01ac806cc803db664abf1efbbad9ede8c02b1e
Caglar <caglarsulun@hotmail.com>
2021-11-28 00:25:49 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(record) {
+    let result = record.find(record => record.result === "W")
+    if(result) {
+      return result.year;
+    } else {
+      return undefined;
+    }
+  }
 
